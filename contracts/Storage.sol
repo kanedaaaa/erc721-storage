c4560a2c08bec02964ba583a353af3d341f63668
Kata <68785244+kanedaaaa@users.noreply.github.com>
2022-10-22 03:08:14 +0200

    feat/ implement store() logic

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -4,37 +4,41 @@ pragma solidity ^0.8.11;
 
 /**
  * NFT Storage contract.
- * Can store any NFT from any contract. 
+ * Can store any NFT from any contract.
  *
  * @author kata
  */
 
 import "./libs/Array.sol";
 
-contract Storage {    
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Storage {
     using Array for uint256[];
-    
+
     /**
      * @notice User info
      * storedTokens: NFT contract address => ID of stored NFT.
+     * timestamps: mapping (contract address => token id => timestamp in unix format).
      * amountStored: Total amount of NFTs stored across every collection.
      */
     struct User {
         mapping(address => uint256[]) storedTokens;
+        mapping(address => mapping(uint256 => uint256)) timestamps;
         uint256 amountStored;
     }
 
-    /** @notice Two way mapping to check User info, And see if msg.sender is owner. */
+    /** @notice Two way mapping to track User info. */
     mapping(address => User) public owners;
-    mapping(address => bool) public isOwner;
 
     event Store(address _contract, uint256[] _id, address _staker);
     event Withdraw(address _contract, uint256[] _id, address _unstaker);
 
     /**
      * @notice External function that wraps internal _store for code clarity.
-     * @param _contract: Address of NFT contract
-     * @param _id: Array of NFT ID's 
+     * @param _contract: Address of NFT contract.
+     * @param _id: Array of NFT ID's.
      */
     function store(address _contract, uint256[] memory _id) external payable {
         if (_id.length != 1) {
@@ -50,14 +54,14 @@ contract Storage {
 
     /**
      * @notice External function that wraps internal _withdraw for code clarity.
-     * @param _contract: Address of NFT contract
-     * @param _id: Array of NFT ID's 
+     * @param _contract: Address of NFT contract.
+     * @param _id: Array of NFT ID's.
      */
     function withdraw(address _contract, uint256[] memory _id) external {
         if (_id.length != 1) {
             for (uint256 i = 0; i < _id.length; ++i) {
                 _withdraw(_contract, _id[i], msg.sender);
-            } 
+            }
         } else {
             _withdraw(_contract, _id[0], msg.sender);
         }
@@ -65,10 +69,35 @@ contract Storage {
         emit Withdraw(_contract, _id, msg.sender);
     }
 
-    function _store(address _contract, uint256 _id, address _user) internal {
+    /**
+     * @notice Store tokens in contract.
+     * @param _user: msg.sender.
+     *
+     * See {store()} for other params.
+     *
+     * With IERC721 `transferFrom`, Contract will request msg.sender's
+     * token ID's, Then increment User info accordingly.
+     */
+    function _store(
+        address _contract,
+        uint256 _id,
+        address _user
+    ) internal {
+        User storage user = owners[_user];
 
+        IERC721(_contract).trasferFrom(_user, address(this), _id);
+
+        user.amountStored += 1;
+        user.timestamps[_contract][_id] = block.timestamp;
+        user.storedTokens[_contract].push(_id);
+        owners[_contract][_id] = _user;
     }
 
-    function _withdraw(address _contract, uint256 _id, address _user) internal {
-    }
+    // TEST STOP //
+
+    function _withdraw(
+        address _contract,
+        uint256 _id,
+        address _user
+    ) internal {}
 }
